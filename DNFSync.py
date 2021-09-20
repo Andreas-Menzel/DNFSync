cfd51665b286328f9a079cc03249ace062e303dc
Andreas Menzel <mail@andreas-menzel.com>
2021-09-20 19:06:30 +0200

    Created DNFSync

---
+++ DNFSync.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import argparse
+import os.path
+import subprocess
+
+### ARGPARSE ###################################################################
+parser = argparse.ArgumentParser(description='Save and restore your DNF apps!', prog='DNFSync')
+
+parser.add_argument('--version', action='version', version='%(prog)s v1.0')
+parser.add_argument('-b', '--backup',
+    action='store_true',
+    help='Create a list of all installed DNF apps.')
+parser.add_argument('-i', '--install',
+    action='store_true',
+    help='Install all DNF apps listed in a file.')
+parser.add_argument('-f', '--file',
+    default="DNFSync__installed_apps.txt",
+    help='Specify the filename where the DNF apps list is stored.')
+
+args = parser.parse_args()
+# end - argparse
+
+
+def get_installed_apps():
+    if not os.path.isfile(args.file):
+        print('File', '"' + args.file + '"', 'does not exist.')
+        return []
+
+    return_string = ""
+    with open(args.file, 'r') as reader:
+        return_string = reader.read()
+
+    return return_string.split('\n')[1:-1]
+
+
+def create_backup():
+    with open(args.file, 'w') as file:
+        subprocess.run(["dnf", "history", "userinstalled"], stdout=file, text=True).stdout
+
+
+def install(apps):
+    args = ["dnf", "install", "-y"]
+    args.extend(apps)
+    subprocess.run(args)
+
+
+if __name__ == '__main__':
+    if args.install:
+        install(get_installed_apps())
+
+    if args.backup:
+        create_backup()
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # DNFSync
+
+Create a backup file of all installed DNF apps.
+```
+DNFSync.py --backup
+```
+
+Install all DNF apps listed in the backup file.
+```
+DNFSync.py --install
+```
